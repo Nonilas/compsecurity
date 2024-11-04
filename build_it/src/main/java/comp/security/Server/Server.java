609b0f2c307ffce482ab86ff924d61a8cdbb3c1b
AchilleasLeiv <a.leivadiotis@ng-sensors.com>
2024-11-04 19:33:09 +0100

    get mapping in the main server

--- build_it/src/main/java/comp/security/Server/Server.java
+++ build_it/src/main/java/comp/security/Server/Server.java
@@ -1,5 +1,14 @@
 package comp.security.Server;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Server {
-    
+    @SuppressWarnings("rawtypes")
+    @GetMapping("/")
+    public ResponseEntity get(){
+        return ResponseEntity.ok("Hello World");
+    }
 }
